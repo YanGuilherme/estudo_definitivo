2be19e26d6d0736d9d0b816c42465fb90bc5a703
Filipe Brener <filipe.brener@ufv.br>
2023-12-05 21:31:00 -0300

    ajuste de docker

--- api.distritos.normal.threads/src/main/java/br/com/api/distritos/repository/RegiaoIntermediariaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.api.distritos.repository;
-
-import br.com.api.distritos.domain.RegiaoIntermediaria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegiaoIntermediariaRepository extends JpaRepository<RegiaoIntermediaria, Long> {
-}
